8652ee524e9f24abaeabd88842d09654d2ec062e
maybeyourfrnd <sanjaykumar.webdev@gmail.com>
2021-09-02 15:41:34 +0100

    adding buttons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
+import Board from './components/Board';
 
 const App = () => {
   return(
   <>
-  <h1>Welcome to React Parcel Micro App!</h1>
-  <p> to get more minimal than this React app.</p>
+  <h1>TIC TAC TOE</h1>
+  <Board />
 </>
   );
 };
