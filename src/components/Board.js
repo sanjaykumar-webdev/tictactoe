ea194fece76c5d2a3177fc5c950ab834b0029e35
maybeyourfrnd <sanjaykumar.webdev@gmail.com>
2021-09-08 13:58:54 +0100

    tictactoe game

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import Square from './Square';
 
-const Board = ({ board, handleSquareClick, winningSquares}) => {
- 
-
+const Board = ({ board, handleSquareClick, winningSquares }) => {
   const renderSquare = position => {
+    const isWinningSquare = winningSquares.includes(position);
 
-    const isWinningSquare = winningSquares.includes(position)
-     return(
-      <Square 
-      value={board[position]} 
-      onClick ={() => handleSquareClick(position)}
-      isWinningSquare={isWinningSquare}
+    return (
+      <Square
+        value={board[position]}
+        onClick={() => handleSquareClick(position)}
+        isWinningSquare={isWinningSquare}
       />
     );
   };
@@ -19,19 +17,19 @@ const Board = ({ board, handleSquareClick, winningSquares}) => {
   return (
     <div className="board">
       <div className="board-row">
-        { renderSquare(0) }
-        { renderSquare(1) }
-        { renderSquare(2) }
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
       </div>
       <div className="board-row">
-      { renderSquare(3) }
-      { renderSquare(4) }
-      { renderSquare(5) }
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
       </div>
       <div className="board-row">
-      { renderSquare(6) }
-      { renderSquare(7) }
-      { renderSquare(8) }
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
       </div>
     </div>
   );
